0788ace4660429af06c65d897c1a043bde920eb2
sabri <mohamed.baazaoui@hp.com>
2022-09-05 11:04:55 +0100

    add second test and refactor

--- GildedRose.Test/GildedRoseTest.cs
+++ GildedRose.Test/GildedRoseTest.cs
@@ -5,17 +5,33 @@ namespace csharp
 {
     [TestFixture]
     public class GildedRoseTest
+
     {
+        private Item DealingWithItems(string name, int sellin, int quality){
+            IList<Item> items = new List<Item>() { new Item { Name=name, Quality=quality,SellIn=sellin} };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            return items[0];
+        }
         [Test]
         public void GiledRose_givenRegularItem_SellInAndQualityLower()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 14, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
+            Item item = DealingWithItems("foo", 15, 25);
+          
             Assert.Multiple(() =>
             {
-                Assert.That(Items[0].SellIn, Is.EqualTo(13));
-                Assert.That(Items[0].Quality, Is.EqualTo(0));
+                Assert.That(item.SellIn, Is.EqualTo(14));
+                Assert.That(item.Quality, Is.EqualTo(25));
+            });
+        }
+        [Test]
+        public void GiledRose_givenRegularItem_PastSellInAndQualityDegradstwiceAsFast()
+        {
+            Item item = DealingWithItems("foo", 0, 25);
+            Assert.Multiple(() =>
+            {
+                Assert.That(item.SellIn, Is.EqualTo(0));
+                Assert.That(item.Quality, Is.EqualTo(23));
             });
         }
     }
