6a372f67d6174e44335dd711aeed0bf96768b811
sabri <mohamed.baazaoui@hp.com>
2022-09-05 12:08:34 +0100

    Extract string Constant

--- GildedRose.Test/GildedRoseTest.cs
+++ GildedRose.Test/GildedRoseTest.cs
@@ -43,56 +43,63 @@ namespace csharp
         [Test]
         public void GiledRose_givenAgedBrie_QualityIncreased()
         {
-            Item item = DealingWithItems("Aged Brie", 15, 25);
+            Item item = DealingWithItems(GildedRose.AGED_BRIE, 15, 25);
             Assert.That(item.Quality, Is.EqualTo(26));
         }
         //he Quality of an item is never more than 50
         [Test]
         public void GiledRose_givenAgedBrie_QualityNeverMoreThan50()
         {
-            Item item = DealingWithItems("Aged Brie", 15, 50);
+            Item item = DealingWithItems(GildedRose.AGED_BRIE, 15, 49);
             Assert.That(item.Quality, Is.EqualTo(50));
         }
         //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
         [Test]
-        public void GiledRose_givenSulfuras_Qualitydecreases()
+        public void GiledRose_givenSulfuras_QualityNeverdecreases()
         {
-            Item item = DealingWithItems("Sulfuras, Hand of Ragnaros", 15, 80);
+            Item item = DealingWithItems(GildedRose.SULFURAS, 15, 80);
+            Assert.That(item.Quality, Is.EqualTo(80));
+        }
+        //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+        [Test]
+        public void GiledRose_givenSulfurasPastSellin_QualityVNeverdecreases()
+        {
+            Item item = DealingWithItems(GildedRose.SULFURAS, -1, 80);
             Assert.That(item.Quality, Is.EqualTo(80));
         }
         //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
         [Test]
         public void GiledRose_givenBackstage_QualityIncreases()
         {
-            Item item = DealingWithItems("Backstage passes to a TAFKAL80ETC concert", 15, 26);
+            Item item = DealingWithItems(GildedRose.BACKSTAGE_PASSES, 15, 26);
             Assert.That(item.Quality, Is.EqualTo(27));
         }
         //	Quality increases by 2 when there are 10 days or less
         [Test]
         public void GiledRose_givenBackstageDaysOut10_QualityIncreaseBy2()
         {
-            Item item = DealingWithItems("Backstage passes to a TAFKAL80ETC concert", 10, 26);
+            Item item = DealingWithItems(GildedRose.BACKSTAGE_PASSES, 10, 26);
             Assert.That(item.Quality, Is.EqualTo(28));
         }
         //	by 3 when there are 5 days 
         [Test]
         public void GiledRose_givenBackstageDaysOut5_QualityIncreaseBy3()
         {
-            Item item = DealingWithItems("Backstage passes to a TAFKAL80ETC concert", 5, 26);
+            Item item = DealingWithItems(GildedRose.BACKSTAGE_PASSES, 5, 26);
             Assert.That(item.Quality, Is.EqualTo(29));
         }
         //  Quality drops to 0 after the concert 
         [Test]
         public void GiledRose_givenBackstage_QualityDropTo0()
         {
-            Item item = DealingWithItems("Backstage passes to a TAFKAL80ETC concert", 0, 26);
+            Item item = DealingWithItems(GildedRose.BACKSTAGE_PASSES, 0, 26);
             Assert.That(item.Quality, Is.EqualTo(0));
         }
         //"Conjured" items degrade in Quality twice as fast as normal items
         [Test]
         public void GiledRose_givenConjured_QualityDegradstwiceAsFast()
         {
-            Item item = DealingWithItems("Conjured", 0, 26);
+            Item item = DealingWithItems("Conjured Mana Cake", 0, 26);
             Assert.That(item.Quality, Is.EqualTo(24));
         }
     }
--- GildedRose.cs
+++ GildedRose.cs
@@ -4,6 +4,9 @@ namespace csharp
 {
     public class GildedRose
     {
+        public const string AGED_BRIE = "Aged Brie";
+        public const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        public const string SULFURAS = "Sulfuras, Hand of Ragnaros";
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -14,11 +17,11 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != AGED_BRIE && Items[i].Name != BACKSTAGE_PASSES)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != SULFURAS)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -30,7 +33,7 @@ namespace csharp
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == BACKSTAGE_PASSES)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -51,20 +54,20 @@ namespace csharp
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != SULFURAS)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != AGED_BRIE)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != BACKSTAGE_PASSES)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != SULFURAS)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
